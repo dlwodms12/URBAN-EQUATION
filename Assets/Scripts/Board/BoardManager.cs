4a937b4eadf6d9ea7d17d55d33b4f0d8b8fa2e8a
Jae Eun <pdw05498@naver.com>
2026-08-28 23:31:17 +0900

    보드 매니저 추가
    -타일을 생성하는 보드 매니저 코드 추가
    -씬에 보드 매니저 추가

--- Assets/Scripts/Board/BoardManager.cs
+++ Assets/Scripts/Board/BoardManager.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public const int BoardSize = 8;
+
+    [SerializeField]
+    private Tile tilePrefab;
+
+    private Tile[,] tiles; // 2차원 배열 선언
+
+    private void Awake()
     {
-        
+        CreateBoard();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateBoard()
     {
-        
+        tiles = new Tile[BoardSize, BoardSize];
+
+        for (int y = 0; y < BoardSize; y++)
+        {
+            for (int x = 0; x < BoardSize; x++)
+            {
+                Tile tile = Instantiate(tilePrefab, transform);
+
+                tile.transform.position = new Vector3(x, 0f, y);
+
+                tile.Initialize(new Vector2Int(x, y));
+
+                tiles[x, y] = tile;
+            }
+        }
+    }
+
+    public Tile GetTile(Vector2Int coordinate)
+    {
+        if (coordinate.x < 0 || coordinate.x >= BoardSize)
+        {
+            return null;
+        }
+
+        if (coordinate.y < 0 || coordinate.y >= BoardSize)
+        {
+            return null;
+        }
+
+        return tiles[coordinate.x, coordinate.y];
     }
 }
