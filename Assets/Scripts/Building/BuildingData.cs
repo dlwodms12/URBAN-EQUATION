2d97caa29584a3d24360a3012915894e9a746f26
Jae Eun <pdw05498@naver.com>
2026-08-28 23:49:31 +0900

    건물 데이터를 저장하기 위한 코드 추가

--- Assets/Scripts/Building/BuildingData.cs
+++ Assets/Scripts/Building/BuildingData.cs
@@ -1,16 +1,42 @@
+using Mono.Cecil;
 using UnityEngine;
 
-public class BuildingData : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(
+    fileName = "BuildingData",
+    menuName = "Urban Equation/Building Data"
+)]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//건물 데이터를 저장
+public class BuildingData : ScriptableObject
+{
+    [Header("Building Information")]
+    [SerializeField]
+    private int buildingCode;
+
+    [SerializeField]
+    private string buildingName;
+
+    [Header("Resource")]
+    [SerializeField]
+    private ResourceType produceResource;
+
+    [SerializeField]
+    private int produceAmount;
+
+    [SerializeField]
+    private ResourceType consumeResource;
+
+    [SerializeField]
+    private int consumeAmount;
+
+    //외부 접근용 프로퍼티
+    public int BuildingCode => buildingCode;
+    public string BuildingName => buildingName;
+
+    // ResourceManager에 정의되어 있는 ResourceType에 접근하기 위한 프로퍼티
+    public ResourceType ProduceResource => produceResource;
+    public int ProduceAmount => produceAmount;
+
+    public ResourceType ConsumeResource => consumeResource;
+    public int ConsumeAmount => consumeAmount;
 }
---
+++ Assets/Scripts/Resource/ResourceManager.cs
@@ -0,0 +1,11 @@
+public enum ResourceType
+{
+    Population, //인구 수
+    Jobs, //일자리
+    Goods, //재화
+    Logistics //물류
+}
+
+public class ResourceManager
+{
+}
