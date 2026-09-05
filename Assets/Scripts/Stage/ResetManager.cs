2b4f313ba89918181b28a6e3abab78575568a937
Jae Eun <pdw05498@naver.com>
2026-09-06 03:54:38 +0900

    스테이지 리셋 기능 추가

--- Assets/Scripts/Stage/ResetManager.cs
+++ Assets/Scripts/Stage/ResetManager.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class ResetManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Manager References")]
+    [SerializeField] private BoardManager boardManager;
+    [SerializeField] private BuildingPlacement buildingPlacement;
+    [SerializeField] private ResourceManager resourceManager;
+    [SerializeField] private StageManager stageManager;
 
-    // Update is called once per frame
-    void Update()
+    [Header("UI References")]
+    [SerializeField] private ClearUI clearUI;
+    [SerializeField] private ComboUI comboUI;
+
+    public void ResetGame()
     {
-        
+        if (boardManager != null)
+        {
+            boardManager.ResetBoard();
+        }
+
+        if (buildingPlacement != null)
+        {
+            buildingPlacement.ResetBuildings();
+        }
+
+        if (resourceManager != null)
+        {
+            resourceManager.ResetResources();
+        }
+
+        if (stageManager != null)
+        {
+            stageManager.ResetStage();
+        }
+
+        if (clearUI != null)
+        {
+            clearUI.ResetUI();
+        }
+
+        if (comboUI != null)
+        {
+            comboUI.ResetUI();
+        }
+
+        Debug.Log("게임이 초기 상태로 리셋되었습니다.");
     }
 }
